63f6f4323600b938f71a8ed133ce3603833b00c9
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-10-29 23:27:10 +0200

    Connect to swapi-service

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import swapiservice from '../api/swapisevice';
 
-const App = () => {
-    return <div>App</div>
+class App extends React.Component {
+    state = {
+        planets: []
+    }
+
+    componentDidMount() {
+        this.onGetPlanets();
+    }
+
+    onGetPlanets = async () => {
+        const response = await swapiservice.get('/planets');
+         this.setState({
+             planets: response.data.results
+         })
+         console.log(this.state.planets)
+    }
+
+    render() {
+        return <div>App</div>
+    }
 }
 
 export default App;
