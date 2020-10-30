7f6b7e9efd0e6afbbe4695a4af9b7dcd0757ae49
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-10-30 18:24:25 +0200

    Create component for Planet details

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import swapiservice from '../api/swapisevice';
 import PlanetList from '../components/PlanetList';
+import PlanetDetails from '../components/PlanetDetails';
 import Header from './Header';
 
 class App extends React.Component {
@@ -32,7 +33,10 @@ class App extends React.Component {
                             render={() => (
                                 <PlanetList planets={this.state.planets}/>
                             )} />
-                        <Route path="/planet/:id" exact />
+                        <Route path="/planet/:id" exact 
+                            render={() => (
+                                <PlanetDetails planets={this.state.planets} />
+                            )}/>
                     </div>
                 </Router>            
             </div>
---
+++ src/components/PlanetDetails.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PlanetList from './PlanetList';
+
+const PlanetDetails = () => {
+    return <div>Planet Details</div>
+};
+
+export default PlanetDetails;
